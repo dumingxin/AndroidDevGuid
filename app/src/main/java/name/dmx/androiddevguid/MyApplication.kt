85bf6687d556eae8155b3fb591ea4263a948a713
dmx <mail@dmx.name>
2017-12-20 18:09:18 +0800

    添加bomb

--- app/src/main/java/name/dmx/androiddevguid/MyApplication.kt
+++ app/src/main/java/name/dmx/androiddevguid/MyApplication.kt
@@ -1,10 +1,14 @@
 package name.dmx.androiddevguid
 
 import android.app.Application
+import cn.bmob.v3.Bmob
 
 /**
  * Created by dmx on 2017/12/12.
  */
 class MyApplication : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        Bmob.initialize(this, "c33ec77b6d3a654581d46b99c6d84f8d")
+    }
 }
