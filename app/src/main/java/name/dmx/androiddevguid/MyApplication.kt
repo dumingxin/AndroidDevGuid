db69e7e07951e0f45777721e325fef1a0d071d94
dmx <mail@dmx.name>
2017-12-21 10:53:39 +0800

    ignore gradle.properties

--- app/src/main/java/name/dmx/androiddevguid/MyApplication.kt
+++ app/src/main/java/name/dmx/androiddevguid/MyApplication.kt
@@ -9,6 +9,6 @@ import cn.bmob.v3.Bmob
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        Bmob.initialize(this, "c33ec77b6d3a654581d46b99c6d84f8d")
+        Bmob.initialize(this, BuildConfig.bombAppKey)
     }
 }
